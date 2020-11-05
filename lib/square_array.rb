13f5dd61f6f11a914ce2721bff8219bc067d609a
Charlie Knight <williamchknight@gmail.com>
2020-11-05 21:03:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,4 @@
 numbers = [1,2,3]
-
-def square_array(array)
-  count = 0 
-  new_numbers = []
-  while count < numbers.length do 
-    new_numbers.push(numbers[count]^2)
-    count += 1 
-  end
-  new_numbers
-end
-
+new_numbers = []
 
 
